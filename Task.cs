d75f11219afbcf083b1380d6f105bc82c878a52f
Ravinsd <145003381+Ravinsd@users.noreply.github.com>
2023-09-14 14:56:09 +0530

    Add files via upload

---
+++ Task.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Octopus.ApiSamples.Models
+{
+    public class Task
+    {
+        [JsonProperty("TaskId")]
+        public string Id { get; set; }
+
+        [JsonProperty("TaskName")]
+        public string Name { get; set; }
+    }
+}
---
+++ TaskDataExportResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Octopus.ApiSamples.Models
+{
+    public class TaskDataExportResult
+    {
+        public int Total { get; set; }
+
+        public int CurrentTotal { get; set; }
+
+        public DataTable DataList { get; set; }
+    }
+}
